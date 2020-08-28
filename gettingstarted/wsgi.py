6178856a7fa35701e226f706ddd497e0ad73e877
Kammy-IO <kamlesh.lokesh@gmail.com>
2020-08-28 14:44:56 +0530

    Update wsgi.py

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -5,7 +5,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +13,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abuse.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+import heroku_bouncer
+application = heroku_bouncer.bouncer(application)
